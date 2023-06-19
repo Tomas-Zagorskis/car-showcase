820288297469549682796e5be31336ba45ae66ac
Tomas Zagorskis <tomzagos@gmail.com>
2023-06-19 18:32:52 +0300

    feat: add props to app button

--- components/AppButton.tsx
+++ components/AppButton.tsx
@@ -1,20 +1,34 @@
 'use client';
 
 import { AppButtonProps } from '@/types';
+import Image from 'next/image';
 
 const AppButton: React.FC<AppButtonProps> = ({
 	title,
 	containerStyles,
 	handleClick,
 	btnType,
+	textStyles,
+	rightIcon,
+	isDisabled,
 }) => {
 	return (
 		<button
-			disabled={false}
+			disabled={isDisabled || false}
 			type={btnType || 'button'}
 			className={`custom-btn ${containerStyles}`}
 			onClick={handleClick}>
-			<span className={`flex-1`}>{title}</span>
+			<span className={`flex-1 ${textStyles}`}>{title}</span>
+			{rightIcon && (
+				<div className='relative w-6 h-6'>
+					<Image
+						src={rightIcon}
+						alt='right icon'
+						fill
+						className='object-contain'
+					/>
+				</div>
+			)}
 		</button>
 	);
 };
